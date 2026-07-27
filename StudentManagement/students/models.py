5bb966eba7ce8a9cbab80dcc9eed8ab9ceec1bf0
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-27 11:34:58 +0530

    start Django setup

--- StudentManagement/students/models.py
+++ StudentManagement/students/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+    course = models.CharField(max_length=100)
+    admission_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
