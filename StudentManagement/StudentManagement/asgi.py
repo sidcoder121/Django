04e16bbac9adb3affc1c2bcd54594a685b988233
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-27 11:04:29 +0530

    create a new django app

--- StudentManagement/StudentManagement/asgi.py
+++ StudentManagement/StudentManagement/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Vars project.
+ASGI config for StudentManagement project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vars.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StudentManagement.settings')
 
 application = get_asgi_application()
