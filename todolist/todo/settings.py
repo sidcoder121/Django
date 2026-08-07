395df3f3081f4f6b7da655cac07ec66c4fac3a02
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-07 12:24:26 +0530

    setup the databse in django

--- todolist/todo/settings.py
+++ todolist/todo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todolist',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo_db',
+        'USER': 'rootdb',
+        'PASSWORD': 'sadaf121',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
